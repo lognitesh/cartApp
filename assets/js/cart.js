639bffa4e86e92b872bf4d85d29b87d895d6ec65
kumarpan <kumarpan@adobe.com>
2021-02-09 18:52:04 +0530

    Tweaks

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -2,13 +2,15 @@ const cartItemCount = document.getElementById('cartItemCount');
 const cartItemsArr = [];
 
 const cart = (items) =>{
-    const cartBtnArr = document.querySelectorAll(".addToCart");
+    const cartBtnArr = document.querySelectorAll(".btn");
     cartBtnArr.forEach((btn)=>{
         btn.addEventListener('click', (e)=>{
             const btnId = e.target.getAttribute('id');
             cartItemsArr.push(items[btnId]);
             console.log(cartItemsArr);
             cartItemCount.innerHTML = cartItemsArr.length;
+
+            (cartItemsArr.length === 0) ? cartItemCount.style.display = "none" : cartItemCount.style.display = "flex";
         });
     })
 };
--- assets/js/item.js
+++ assets/js/item.js
@@ -1,13 +1,13 @@
 const renderItem = (data) =>{
     return `<li id="${data.id}">
-    <img src="${data.image}" />
+    <img src="${data.image}" alt="${data.name}" title="${data.name}" />
     <h2>${data.name}</h2>
     <div class="info">
-        <span class="price">${data.price.actual}</span>
-        <span class="discount">${data.price.display}</span>
+        <span class="price">$${data.price.actual}</span>
+        <span class="discount">$${data.price.display}</span>
         <span class="off">${data.discount}% off</span>
     </div>
-    <button class="addToCart" id="${data.id}">Add to Cart</button>
+    <button class="btn" id="${data.id}">Add to Cart</button>
 </li>`;
 };
 
